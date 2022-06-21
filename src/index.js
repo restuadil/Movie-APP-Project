4ca619f553832449a9cdda8aaa6cdee47a0ad823
Restu Adil <restuadil09@gmail.com>
2022-06-21 09:45:51 +0700

    commit on master

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 
@@ -10,9 +10,9 @@ ReactDOM.render(
   <React.StrictMode>
     {}
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
